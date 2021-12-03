a9cfe9b25a0ac337828604c411964cdf538dbee2
Willi Mutschler <willi@mutschler.eu>
2021-12-03 09:41:18 +0100

    Create restic prune and check scripts

--- restic-check.sh
+++ restic-check.sh
@@ -3,9 +3,7 @@ eval $(grep -v -e '^#' /home/wmutschl/scripts/.env | xargs -I {} echo export \'{
 echo "*****************************************************"
 echo $(date)
 echo " "
-echo "Pruning and checking restic repo on WASABI"
-
-restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 1 --prune
+echo "Checking restic repo: $RESTIC_REPOSITORY"
 restic check --read-data
 echo " "
 echo "Finished"
---
+++ restic-prune.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+eval $(grep -v -e '^#' /home/wmutschl/scripts/.env | xargs -I {} echo export \'{}\')
+echo "*****************************************************"
+echo $(date)
+echo " "
+echo "Pruning restic repo: $RESTIC_REPOSITORY"
+restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 1 --prune
+echo " "
+echo "Finished"
+echo "*****************************************************"
