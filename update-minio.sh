7e81c9d2b1e413027c154604b05e6ffadb1ee791
Willi Mutschler <willi@mutschler.eu>
2022-03-02 11:41:11 +0100

    Improved minio update command for systemctl restart failures

--- update-minio.sh
+++ update-minio.sh
@@ -1,8 +1,11 @@
-sudo systemctl stop minio
-sudo systemclt disable minio
+#!/bin/bash
+systemctl stop minio
+systemctl disable minio
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio
-sudo chown user-minio. minio
-sudo chmod +x minio
-sudo mv minio /usr/local/bin
-sudo systemctl enable minio
-sudo systemctl start minio
+chown user-minio. minio
+chmod +x minio
+mv minio /usr/local/bin
+systemctl reset-failed minio
+systemctl enable minio
+systemctl start minio
+systemctl status minio
