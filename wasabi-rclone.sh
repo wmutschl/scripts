d26afbe6d0bae75a8f75378073032e2bb1f517ff
Willi Mutschler <willi@mutschler.eu>
2022-03-02 13:38:21 +0100

    Remove unused scripts

--- wasabi-rclone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-eval $(grep -v -e '^#' /home/wmutschl/scripts/.env | xargs -I {} echo export \'{}\')
-echo "*****************************************************"
-echo $(date)
-echo " "
-
-rclone --config $WASABI_RCLONE_CONF sync wasabi:$WASABI_BUCKET $EXTERNAL_HDD/$WASABI_BUCKET -P
-if [ $? -ne 0 ]; then url=$baseurl/fail; fi
