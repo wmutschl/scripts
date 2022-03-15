6cb2d5f0a224e3175821d7697901be92f1816c99
Willi Mutschler <willi@mutschler.eu>
2022-03-15 20:26:50 +0100

    More restic cache cleanups

--- restic-check.sh
+++ restic-check.sh
@@ -8,6 +8,7 @@ export RESTIC_REPOSITORY=${RESTIC_CHECK_PATH}
 echo " "
 echo "Checking restic repo: $RESTIC_REPOSITORY"
 
+restic cache --cleanup
 #restic check
 restic check --read-data
 #restic check --read-data-subset=1/5
@@ -15,6 +16,7 @@ restic check --read-data
 #restic check --read-data-subset=3/5
 #restic check --read-data-subset=4/5
 #restic check --read-data-subset=5/5
+restic cache --cleanup
 
 echo " "
 echo "Finished"
--- restic-prune.sh
+++ restic-prune.sh
@@ -6,9 +6,9 @@ echo $(date)
 export RESTIC_REPOSITORY=${RESTIC_PRUNE_PATH}
 
 echo "Pruning restic repo: $RESTIC_REPOSITORY"
-
+restic cache --cleanup
 restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 1 --prune
-
+restic cache --cleanup
 echo " "
 echo "Finished"
 echo "*****************************************************"
