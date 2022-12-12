77cc079c874680b99f6131f2adb7ca4e69f81750
Willi Mutschler <willi@mutschler.eu>
2022-12-12 09:39:28 +0100

    Improved windows scripts

--- xps-restic.ps1
+++ xps-restic.ps1
@@ -1,3 +1,5 @@
+# DIESE DATEI ÜBER RECHTSKLICK - MIT POWERSHELL AUSFÜHREN (da admin Rechte benötigt werden)
+# OPEN THIS FILE USING RIGHT-CLICK - RUN WITH POWERSHELL (as we need admin rights)
 param([switch]$Elevated)
 
 function Test-Admin {
@@ -19,6 +21,8 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 # temporarily change to the correct folder
 Push-Location $dir
+restic cache --cleanup
 restic -r ../xps_backup backup --use-fs-snapshot --exclude-file=restic-exclude-windows.txt --verbose C:\
+restic cache --cleanup
 # now back to previous directory
 Pop-Location
